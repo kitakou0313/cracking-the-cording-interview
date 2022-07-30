70f301a24e009247bd74526d890fcdf5f5eb6c24
kitakou0313 <kitakou0313@gmail.com>
2022-07-30 14:18:03 +0000

    Add code for 1 - 1

--- chap1/1.py
+++ chap1/1.py
@@ -16,6 +16,20 @@ def is_specific(string: str):
     return True
 
 
+def is_specific_without_other_space(string: str):
+    """
+    stringに重複する文字が無いか判定
+    追加メモリ無し
+    """
+    sorted_string = sorted(string)
+
+    for ind in range(len(sorted_string) - 1):
+        if (sorted_string[ind] == sorted_string[ind + 1]):
+            return False
+
+    return True
+
+
 class Test(unittest.TestCase):
     """
     test class
@@ -28,6 +42,14 @@ class Test(unittest.TestCase):
         self.assertEqual(is_specific("abcdefg"), True)
         self.assertEqual(is_specific("aaaaaa"), False)
         self.assertEqual(is_specific(""), True)
+
+    def test_is_specific_without_other_space(self):
+        """
+        is_specific_without_other_spaceのtest
+        """
+        self.assertEqual(is_specific_without_other_space("abcdefg"), True)
+        self.assertEqual(is_specific_without_other_space("aaaaaa"), False)
+        self.assertEqual(is_specific_without_other_space(""), True)
 
 
 if __name__ == "__main__":
