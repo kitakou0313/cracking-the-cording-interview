864e98fbc26ebfbd3639ac5dc30b5460463c7706
kitakou0313 <kitakou0313@gmail.com>
2022-08-07 01:56:13 +0000

    Add code for linked list node

---
+++ libs/linked_list.py
@@ -0,0 +1,10 @@
+class LinkedListNode(object):
+    """
+    連結リストのノード
+    """
+
+    def __init__(self, val: int, nxt_node: LinkedListNode, prev_node: LinkedListNode):
+        """
+        docstring
+        """
+        pass
