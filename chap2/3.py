1f9da5a86cb85ba591ce2d94dd80fc2439b43142
kitakou0313 <kitakou0313@gmail.com>
2022-08-14 05:53:26 +0000

    Fix corner case

--- chap2/3.py
+++ chap2/3.py
@@ -2,12 +2,18 @@ from libs.linked_list import SingleLinkedList, LinkedListNode
 import unittest
 
 
-def removeNode(lList: SingleLinkedList, node: LinkedListNode):
+def remove_node(lList: SingleLinkedList, node: LinkedListNode):
     """
     与えられたリストのNodeを削除
     """
+    # 末尾の削除の場合
     if node.nxt is None:
-        pass
+        current_node = lList.head
+        while current_node.nxt != node:
+            current_node = current_node.nxt
+
+        current_node.nxt = None
+
     else:
         node.val = node.nxt.val
         node.nxt = node.nxt.nxt
@@ -16,6 +22,7 @@ def removeNode(lList: SingleLinkedList, node: LinkedListNode):
 class Test(unittest.TestCase):
     testCases = (
         ([10, 20, 30, 40, 50], 2, [10, 20, 40, 50]),
+        ([10, 20, 30, 40, 50], 4, [10, 20, 30, 40]),
     )
 
     def test1(self, ):
@@ -31,7 +38,7 @@ class Test(unittest.TestCase):
             lList.add_multi_node(list_vals[node_val_ind+1:])
 
             self.assertEqual(lList.get_vals(), list_vals)
-            removeNode(lList, middle_node)
+            remove_node(lList, middle_node)
             self.assertEqual(lList.get_vals(), expected)
 
 
