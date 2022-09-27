7ce62daa7ad574eac673f4a8a3ee08172d10e671
kitakou0313 <kitakou0313@gmail.com>
2022-09-27 13:49:10 +0000

    Add ans of 4-2

--- chap4/2.py
+++ chap4/2.py
@@ -5,8 +5,22 @@ from libs.tree import BinTreeNode, cal_bin_tree_height
 def create_minimal_height_bin_tree(vals_list: list) -> BinTreeNode:
     """
     与えられた要素を用いて高さが最小の二分探索木を生成
+    vals_listはすべてソート済みのため、半分に分割して二分木にしていく
     """
-    pass
+    if len(vals_list) == 0:
+        return None
+
+    middle_ind = len(vals_list)//2
+
+    left_tree_vals = vals_list[:middle_ind]
+    right_tree_vals = vals_list[middle_ind+1:]
+
+    root_node: BinTreeNode = BinTreeNode(vals_list[middle_ind])
+
+    root_node.set_left_node(create_minimal_height_bin_tree(left_tree_vals))
+    root_node.set_right_node(create_minimal_height_bin_tree(right_tree_vals))
+
+    return root_node
 
 
 class Test(unittest.TestCase):
@@ -15,7 +29,8 @@ class Test(unittest.TestCase):
         test_case = [
             ([1, 2, 3], 2),
             ([1, 2], 2),
-            ([1, 2, 3, 4], 3)
+            ([1, 2, 3, 4], 3),
+            ([1, 2, 3, 4, 5], 3)
         ]
 
         for input_case, expected in test_case:
