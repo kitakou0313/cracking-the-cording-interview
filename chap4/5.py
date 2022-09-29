7df8e9e3735078f9e88443f151537a4dd1a71abb
kitakou0313 <kitakou0313@gmail.com>
2022-09-29 01:59:55 +0000

    Add answer of chap 4 5

--- chap4/5.py
+++ chap4/5.py
@@ -24,6 +24,24 @@ def is_BST_helper(root_node: BinTreeNode) -> SearchRes:
     is_having_left_tree = not(root_node.get_left_node() is None)
     is_having_right_tree = not(root_node.get_right_node() is None)
 
+    search_res_left_tree = is_BST_helper(root_node.get_left_node()) if is_having_left_tree else SearchRes(
+        is_bst=True, min_val=float("inf"), max_val=-float("inf")
+    )
+    search_res_right_tree = is_BST_helper(root_node.get_right_node()) if is_having_right_tree else SearchRes(
+        is_bst=True, min_val=float("inf"), max_val=-float("inf")
+    )
+
+    bst_check_res = (search_res_left_tree.is_bst and search_res_right_tree.is_bst) and (
+        search_res_left_tree.max_val < root_node.get_val(
+        ) and root_node.get_val() <= search_res_right_tree.min_val
+    )
+
+    return SearchRes(
+        max_val=max(root_node.get_val(), search_res_right_tree.max_val),
+        min_val=min(root_node.get_val(), search_res_left_tree.min_val),
+        is_bst=bst_check_res
+    )
+
 
 def is_BST(root_node: BinTreeNode) -> bool:
     """
@@ -48,23 +66,23 @@ class Test(unittest.TestCase):
 
         self.assertEqual(is_BST(bst), True)
 
-        notBst1 = BinTreeNode(val=5)
-        notBst1.set_left_node(
+        not_bst_1 = BinTreeNode(val=5)
+        not_bst_1.set_left_node(
             BinTreeNode(6)
         )
-        notBst1.set_right_node(
+        not_bst_1.set_right_node(
             BinTreeNode(10)
         )
-        self.assertEqual(is_BST(notBst1), False)
+        self.assertEqual(is_BST(not_bst_1), False)
 
-        notBst = BinTreeNode(val=5)
-        notBst.set_left_node(
+        not_bst_2 = BinTreeNode(val=5)
+        not_bst_2.set_left_node(
             BinTreeNode(1)
         )
-        notBst.set_right_node(
+        not_bst_2.set_right_node(
             BinTreeNode(4)
         )
-        self.assertEqual(is_BST(notBst), False)
+        self.assertEqual(is_BST(not_bst_2), False)
 
         Bst = BinTreeNode(val=5)
         Bst.set_left_node(
@@ -72,11 +90,21 @@ class Test(unittest.TestCase):
         )
         self.assertEqual(is_BST(Bst), True)
 
-        notBst = BinTreeNode(val=5)
-        notBst.set_left_node(
+        not_bst_2 = BinTreeNode(val=5)
+        not_bst_2.set_left_node(
             BinTreeNode(10)
         )
-        self.assertEqual(is_BST(notBst), False)
+        self.assertEqual(is_BST(not_bst_2), False)
+
+        not_bst_3 = BinTreeNode(10)
+
+        not_bst_3.set_left_node(BinTreeNode(1))
+
+        not_bst_3.set_right_node(BinTreeNode(50))
+        not_bst_3.get_right_node().set_left_node(BinTreeNode(60))
+        not_bst_3.get_right_node().set_left_node(BinTreeNode(70))
+
+        self.assertEqual(is_BST(not_bst_3), False)
 
 
 if __name__ == "__main__":
