6e5e7b52541e85ec2361cb2cc8e2674a0f72981d
kitakou0313 <kitakou0313@gmail.com>
2022-10-03 00:06:25 +0000

    Add ans of 8-3

--- chap8/3.py
+++ chap8/3.py
@@ -15,6 +15,32 @@ def find_magic_index(array: list) -> int:
     return NOT_FOUND
 
 
+def find_magic_index_with_bin_search(array: list) -> int:
+    """
+    indexと値が等しいインデックスを返す
+    二分探索する
+    O(log(len(array)))
+    """
+    def is_ok(ind, val):
+        """
+        ind <= valか検証
+        """
+        return ind <= val
+
+    # ind <= valが成立する最小のindを探索し、それがind == valか検証
+    ng = -1
+    ok = len(array)
+
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(mid, array[mid]):
+            ok = mid
+        else:
+            ng = mid
+
+    return ok if array[ok] == ok else NOT_FOUND
+
+
 class Test(unittest.TestCase):
     def test1(self):
         test_case = [
@@ -23,6 +49,15 @@ class Test(unittest.TestCase):
         ]
         for input_case, excepted in test_case:
             self.assertEqual(find_magic_index(input_case), excepted)
+
+    def test2(self):
+        test_case = [
+            ([-11, 0, 2, 6, 7, 10], 2),
+            ([-11, -10, -9, 10, 11], NOT_FOUND)
+        ]
+        for input_case, excepted in test_case:
+            self.assertEqual(
+                find_magic_index_with_bin_search(input_case), excepted)
 
 
 if __name__ == "__main__":
