82168e99404ff9595b1b97c4377d6eef1b928167
kitakou0313 <kitakou0313@gmail.com>
2023-01-01 03:58:47 +0000

    Fix 8_11

--- chap8/11.py
+++ chap8/11.py
@@ -17,17 +17,41 @@ def helper_func(value: int, coins: list, memo: dict) -> int:
     return res
 
 
+def helper_func_without_memo(value: int, coins: list) -> set[tuple]:
+    """
+    メモ化した関数
+    """
+    res: set[tuple] = set([])
+
+    for coin_id, coin_val in enumerate(coins):
+        lasted_value = value - coin_val
+
+        if lasted_value < 0:
+            continue
+
+        res_with_lasted_values = helper_func_without_memo(
+            lasted_value, coins) if lasted_value != 0 else set([tuple([0 for _ in range(len(coins))])])
+
+        for payment_way in res_with_lasted_values:
+            tmp = list(payment_way)
+            tmp[coin_id] += 1
+            res.add(tuple(tmp))
+
+    return res
+
+
 def calculate_coins(value: int, coins: list) -> int:
     """
     与えられたコインでの支払い方の総数を返す
     """
     memo = {}
-    memo[0] = 1
-    return helper_func(value, coins, memo)
+    # memo[0] = 1
+    # return helper_func(value, coins, memo)
+    return len(helper_func_without_memo(value, coins))
 
 
 class Test(unittest.TestCase):
-    def test1(self):
+    def test_1(self):
         test_cases = [
             (1, 1),
             (4, 1),
