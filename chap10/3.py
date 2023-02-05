fd639f4c0735873a8bbf124355841df18a60ee2c
kitakou0313 <kitakou0313@gmail.com>
2023-02-05 01:09:15 +0000

    Add ans for rotated array

--- chap10/3.py
+++ chap10/3.py
@@ -1,4 +1,5 @@
 import unittest
+import bisect
 
 
 def search_trg_num_in_rotated_array(array: list[int], trg_num: int):
@@ -17,8 +18,7 @@ def search_trg_num_in_rotated_array(array: list[int], trg_num: int):
         # 回転されてないケース
         # 2分探索でOK
         # O(logn)
-        pass
-
+        trg_num_ind = bisect.bisect_left(array, trg_num)
     return trg_num_ind
 
 
@@ -33,13 +33,17 @@ class Test(unittest.TestCase):
         """
         array = [55, 60, 65, 70, 75, 80, 85,
                  90, 95, 15, 20, 25, 30, 35, 40, 45]
+        not_rotated_array = sorted(array)
         test_cases = [
-            (55, 0), (60, 1), (90, 7), (95, 8), (15, 9), (30, 12), (45, 15)
+            55, 60, 90, 95, 15, 30, 45
         ]
 
-        for trg_num, ans in test_cases:
+        for trg_num in test_cases:
+            # self.assertEqual(search_trg_num_in_rotated_array(
+            #     array=array, trg_num=trg_num), array.index(trg_num))
             self.assertEqual(search_trg_num_in_rotated_array(
-                array=array, trg_num=trg_num), ans)
+                array=not_rotated_array, trg_num=trg_num
+            ), not_rotated_array.index(trg_num))
 
 
 if __name__ == "__main__":
