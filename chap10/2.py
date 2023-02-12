d36cd423b8a5aba1080735c734594cded8430ebb
kitakou0313 <kitakou0313@gmail.com>
2023-02-12 14:10:30 +0000

    Add ans for chap10 2

---
+++ chap10/2.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+def group_anagrams(strings: str) -> list[str]:
+    """
+    docstring
+    """
+    anagram_dict: dict[str, list[str]] = {}
+
+    for string in strings:
+        sorted_str = "".join(sorted(string))
+
+        if sorted_str not in anagram_dict:
+            anagram_dict[sorted_str] = []
+
+        anagram_dict[sorted_str].append(string)
+
+    sorted_keys = sorted(anagram_dict.keys())
+
+    res = []
+    for anagram in sorted_keys:
+        res += sorted(anagram_dict[anagram])
+    return res
+
+
+class Test(unittest.TestCase):
+    def test_group_anagrams(self):
+        strings = ["cat", "bat", "rat", "arts", "tab", "tar", "car", "star"]
+
+        self.assertEqual(group_anagrams(sorted(strings)),
+                         ['arts', 'star', 'bat', 'tab', 'car', 'cat', 'rat', 'tar'])
+
+
+if __name__ == "__main__":
+    unittest.main()
